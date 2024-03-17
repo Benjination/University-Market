5a7b109bcf23266dceb3b35ad773f514a1636360
Benjamin Niccum <btn1609@mavs.uta.edu>
2024-03-17 18:13:50 -0500

    Sing up is now adding users to database. Needs more work.

--- UniversityMarket/app/src/main/java/com/example/universitymarket/Login.java
+++ UniversityMarket/app/src/main/java/com/example/universitymarket/Login.java
@@ -2,24 +2,51 @@ package com.example.universitymarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.os.Handler;
+import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.universitymarket.objects.User;
+import com.example.universitymarket.utilities.Network;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Date;
 
 public class Login extends AppCompatActivity {
 
+    private static final String TAG = "Login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        FirebaseAuth mAuth;
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
 
 
 
@@ -29,10 +56,10 @@ public class Login extends AppCompatActivity {
 
         TextView invalid = findViewById(R.id.invalidEmail);
         invalid.setVisibility(View.INVISIBLE);
-        TextView emailStudent = findViewById(R.id.emailSent);
-        emailStudent.setVisibility(View.INVISIBLE);
-        TextView emailMod = findViewById(R.id.emailSentMod);
-        emailMod.setVisibility(View.INVISIBLE);
+        //TextView emailStudent = findViewById(R.id.emailSent);
+        //emailStudent.setVisibility(View.INVISIBLE);
+        //TextView emailMod = findViewById(R.id.emailSentMod);
+        //emailMod.setVisibility(View.INVISIBLE);
 
 
         //Buttons
@@ -49,18 +76,20 @@ public class Login extends AppCompatActivity {
         });
 
         gotit.setOnClickListener(v -> {
-            emailStudent.setVisibility(View.INVISIBLE);
+            //emailStudent.setVisibility(View.INVISIBLE);
             invalid.setVisibility(View.INVISIBLE);
             gotit.setVisibility(View.INVISIBLE);
             emailBox.setVisibility(View.VISIBLE);
             passwordBox.setVisibility(View.VISIBLE);
             login.setVisibility(View.VISIBLE);
-            emailMod.setVisibility(View.INVISIBLE);
+            //emailMod.setVisibility(View.INVISIBLE);
             emailBox.setText("");
             passwordBox.setText("");
         });
 
         login.setOnClickListener(v -> {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
             emailBox.setVisibility(View.INVISIBLE);
             passwordBox.setVisibility(View.INVISIBLE);
             login.setVisibility(View.INVISIBLE);
@@ -68,21 +97,51 @@ public class Login extends AppCompatActivity {
             String email = emailBox.getText().toString();
             String password = passwordBox.getText().toString();
             String domain = email.substring(email.indexOf("@") + 1);
+
             if(domain.equals(domain1) && !password.equals(""))
             {
                 emailBox.setVisibility(View.INVISIBLE);
                 passwordBox.setVisibility(View.INVISIBLE);
                 login.setVisibility(View.INVISIBLE);
-                emailStudent.setVisibility(View.VISIBLE);
+                //emailStudent.setVisibility(View.VISIBLE);
                 gotit.setVisibility(View.VISIBLE);
                 System.out.println("User is a student.");
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //updateUI(user);
+                                User currUser = new User(email);
+                                currUser.setId(email);
+                                Date date = new Date();
+                                currUser.setAbout(date.toString(),null, null, null, email, email);
+                                Network.setUser(Login.this, currUser, false, null);
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(Login.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                Toast.makeText(Login.this, "If you have already created an account, Try signing in instead",
+                                        Toast.LENGTH_LONG).show();
+
+                                //updateUI(null);
+                            }
+                        }
+                    });
             }
             else if(domain.equals(domain2) && !password.equals(""))
             {
                 emailBox.setVisibility(View.INVISIBLE);
                 passwordBox.setVisibility(View.INVISIBLE);
                 login.setVisibility(View.INVISIBLE);
-                emailMod.setVisibility(View.VISIBLE);
+                //emailMod.setVisibility(View.VISIBLE);
                 gotit.setVisibility(View.VISIBLE);
                 System.out.println("User is a faculty/staff.");
             }
@@ -96,5 +155,7 @@ public class Login extends AppCompatActivity {
 
 
     }
+
+
 
 }
