1cd8fbf8379331e5abe79dd592661b90606c6d10
275RR <36821077+275RR@users.noreply.github.com>
2024-04-09 00:57:18 -0500

    added ActiveToPOJO

--- UniversityMarket/app/src/main/java/com/example/universitymarket/fragments/viewPostFragment.java
+++ UniversityMarket/app/src/main/java/com/example/universitymarket/fragments/viewPostFragment.java
@@ -1,50 +1,28 @@
 package com.example.universitymarket.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.telecom.Call;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
-import com.example.universitymarket.DashboardActivity;
-import com.example.universitymarket.Login;
 import com.example.universitymarket.R;
-import com.example.universitymarket.SignIn;
-import com.example.universitymarket.adapters.PostGVAdapter;
 import com.example.universitymarket.globals.actives.ActiveUser;
 import com.example.universitymarket.objects.Post;
 import com.example.universitymarket.objects.User;
 import com.example.universitymarket.utilities.Callback;
 import com.example.universitymarket.utilities.Data;
 import com.example.universitymarket.utilities.Network;
-import com.example.universitymarket.utilities.PostModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class viewPostFragment extends Fragment {
 
@@ -56,9 +34,6 @@ public class viewPostFragment extends Fragment {
 
     private View viewSinglePost;
     private int currentIndex = 0;
-
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -86,22 +61,12 @@ public class viewPostFragment extends Fragment {
                 System.out.println(ActiveUser.watch_ids);
             }
             else {
-
                 ActiveUser.watch_ids.add(String.valueOf(this.postId));
                 addWL.setText("Remove from Watchlist");
                 System.out.println(ActiveUser.watch_ids);
             }
-            User user = new User(ActiveUser.email);
-            user.setDateCreated(ActiveUser.date_created);
-            user.setFirstName(ActiveUser.first_name);
-            user.setLastName(ActiveUser.last_name);
-            user.setEmail(ActiveUser.email);
-            user.setDeactivated(ActiveUser.deactivated);
-            //user.setWatchIds(ActiveUser.watch_ids);
-            //user.setTransactIds(ActiveUser.transact_ids);
-            //user.setPostIds(ActiveUser.post_ids);
-            user.setInteractions(ActiveUser.watch_ids, ActiveUser.transact_ids, ActiveUser.post_ids);
-            Network.setUser(requireActivity(), user, false, new Callback<User>() {
+
+            Network.setUser(requireActivity(), Data.activeUserToPOJO(), false, new Callback<User>() {
                 @Override
                 public void onSuccess(User result) {
                     Toast.makeText(requireActivity(), "Updated",
@@ -113,7 +78,6 @@ public class viewPostFragment extends Fragment {
                     Log.e("setUser", error.getMessage());
                 }
             });
-
         });
 
         // Configure the view post fragment
@@ -125,7 +89,6 @@ public class viewPostFragment extends Fragment {
     private void configureViewPostFragment(View view, String postID) {
         fetchPostAndPopulate(postID, view);
     }
-
 
     private void fetchPostAndPopulate(String postID, View view) {
         // Example fetch operation
@@ -141,8 +104,6 @@ public class viewPostFragment extends Fragment {
                 postTitle.setText("$" + result.getListPrice() + " - " + result.getItemTitle());
                 postDescription.setText("Item Description: \n\n\t\t\t" + result.getItemDescription());
                 sellerInfo.setText("Seller Contact: \n\n\t\t\t" + result.getAuthorEmail());
-
-
 
                 // Set up ImageSwitcher
                 postImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
